9a1e3ed01b920c29fa8966541e529002caab35de
Victor Andrieu <victor.andrieu@student.ecp.fr>
2016-08-10 23:06:43 +0200

    fbsdk android setup https://github.com/facebook/react-native-fbsdk

--- android/app/src/main/java/com/androidbetbash/MainActivity.java
+++ android/app/src/main/java/com/androidbetbash/MainActivity.java
@@ -3,6 +3,8 @@ package com.androidbetbash;
 import com.facebook.react.ReactActivity;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
+import android.content.Intent;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,5 +14,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "AndroidBetBash";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
