3c22fda1e4e58147320aa78cde36f416868b5388
Victor Andrieu <victor.andrieu@student.ecp.fr>
2016-08-27 20:25:43 +0200

    useless old root file

--- index.android.js
+++ index.android.js
@@ -1,53 +1,19 @@
 import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import App from './src/containers/App';
+import configureStore from './src/store/configureStore';
 
-const ReactNative = require('react-native');
-const { AppRegistry, StyleSheet, Text, View, Image } = ReactNative;
-// const FBSDK = require('react-native-fbsdk');
-// const { LoginButton, AccessToken } = FBSDK;
-const FacebookLogin = require('./src/components/FacebookLogin.js');
+const store = configureStore();
 
 class AndroidBetBash extends Component {
   render() {
     return (
-      <Image
-        source={ require('./assets/images/jeteedecran.png') }
-        style={ styles.imageBgContainer }>
-        <View style={ styles.container }>
-          <FacebookLogin />
-        </View>
-        <View style={ styles.bottomContainer }>
-          <Text style={ styles.instructions }>
-            or login using your email
-          </Text>
-        </View>
-      </Image>
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  imageBgContainer: {
-    flex: 1,
-    width: null,
-    height: null,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: 'transparent',
-  },
-  bottomContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: 'transparent',
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#FFFFFF'
-  },
-});
 
 AppRegistry.registerComponent('AndroidBetBash', () => AndroidBetBash);
