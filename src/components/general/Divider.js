6f0c8235cc3f26054f999afe64503428f5a4b9b6
Victor Andrieu <victor.andrieu@student.ecp.fr>
2017-01-22 19:12:47 +0100

    divider component

---
+++ src/components/general/Divider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import styles from 'BetBash/src/assets/styles/main'
+
+const Divider = () => (
+  <View style={styles.divider} />
+)
+
+export default Divider
